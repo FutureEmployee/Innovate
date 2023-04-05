f6711009f13de66ed1d508342b40f7ad3651df45
FutureEmployee <96780418+FutureEmployee@users.noreply.github.com>
2023-04-05 16:17:04 -0400

    Assets grid

--- Innovate/ContentView.swift
+++ Innovate/ContentView.swift
@@ -8,19 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let items = Array(1...13).map({ "image\($0)" })
+    
+    let layout = [
+        GridItem(.adaptive(minimum: 100))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(spacing: 0) {
+            HStack{
+                Text("Viz Media").fontWeight(.bold).font(.title).foregroundColor(.white)
+                Button(action: {
+                }) {
+                    Image(systemName: "magnifyingglass").foregroundColor(.white).padding(10)
+                }.background(Color.black)
+                    .clipShape(Circle())
+                
+            }
+            
+            NavigationView {
+                ScrollView(.horizontal) {
+                    LazyVGrid(columns: layout, content: {
+                        ForEach(items, id: \.self) { item in
+                            VStack {
+                                Button {
+                                    
+                                } label: {
+                                    Image(item)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .border(Color.secondary)
+                                        .cornerRadius(12)
+                                }
+                                Spacer()
+                                .foregroundColor(Color.blue)
+                                .padding()
+                            }
+                        }
+                    })
+                }
+              
+                
+                
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .previewDisplayName("light")
+            ContentView()
+                .previewDisplayName("dark")
+                .preferredColorScheme(.dark)
+        }
     }
+    
+    
+    
 }
