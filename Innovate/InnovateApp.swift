bfc3d94443346ee2170e8a3b93a9c50513d01add
FutureEmployee <96780418+FutureEmployee@users.noreply.github.com>
2023-04-05 08:59:03 -0400

    Initial Commit

---
+++ Innovate/InnovateApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnovateApp.swift
+//  Innovate
+//
+//  Created by Marceles Moore on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct InnovateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
